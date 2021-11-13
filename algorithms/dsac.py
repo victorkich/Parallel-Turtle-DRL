8e7034a1d598779602a1d7d57a778e2ac6438103
victorkich <victorkich@yahoo.com.br>
2021-11-13 16:09:15 -0300

    Changes by alisson

--- algorithms/dsac.py
+++ algorithms/dsac.py
@@ -182,7 +182,8 @@ class LearnerDSAC(object):
             policy_loss = torch.min(actor_loss_1, actor_loss_2)
 
         policy_loss = policy_loss * torch.from_numpy(self.value_net_1.z_atoms).float().to(self.device)
-        policy_loss = torch.sum(policy_loss, dim=1)
+        print(policy_loss.item())
+        policy_loss = torch.sum(policy_loss.squeeze(0))
         policy_loss = policy_loss.mean()
 
         self.policy_optimizer.zero_grad()
