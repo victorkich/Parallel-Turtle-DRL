7e5b6b25aa18a1aaeda64e7e939ca12e149d84f6
victorkich <victorkich@yahoo.com.br>
2021-11-16 19:25:36 -0300

    Changes by alisson

--- algorithms/dsac2.py
+++ algorithms/dsac2.py
@@ -64,7 +64,7 @@ class LearnerDSAC(object):
         self.use_automatic_entropy_tuning = config['use_automatic_entropy_tuning']
         if self.use_automatic_entropy_tuning:
             self.target_entropy = -torch.tensor(np.prod(config['action_dim']).item()).to(self.device)  # heuristic value from Tuomas
-            self.log_alpha = torch.zeros(1, requires_grad=True).to(self.device)
+            self.log_alpha = torch.nn.Parameter(torch.tensor([0.0], requires_grad=True).to(self.device))
             self.alpha_optimizer = optim.Adam([self.log_alpha], lr=policy_lr)
         else:
             self.alpha = config['alpha']
