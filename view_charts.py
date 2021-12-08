9b99b622d781fcd7d7ef3c0bdb8be29ef5687b66
victorkich <victorkich@yahoo.com.br>
2021-12-08 16:12:43 -0300

    Add .world and .xacro

--- view_charts.py
+++ view_charts.py
@@ -7,13 +7,22 @@ import json
 import os
 
 path = os.path.dirname(os.path.abspath(__file__)) + '/saved_models/'
-list_dir = os.listdir(path)
+list_dir = sorted(os.listdir(path))
+l1 = list()
+l2 = list()
+for i in range(len(list_dir)):
+    if not i % 2 == 0:
+        l1.append(list_dir[i])
+    else:
+        l2.append(list_dir[i])
+list_dir = l1 + l2
 
 n = 75  # the larger n is, the smoother curve will be
 b = [1.0 / n] * n
 a = 1
 
 color = {0: 'firebrick', 1: 'tomato', 2: 'peru', 3: 'gold', 4: 'dodgerblue', 5: 'springgreen', 6: 'indigo', 7: 'deeppink'}
+
 for c, directory in tqdm(enumerate(list_dir), total=len(list_dir)):
     with open(path+directory+'/writer_data.json') as f:
         data = json.load(f)
@@ -28,13 +37,14 @@ for c, directory in tqdm(enumerate(list_dir), total=len(list_dir)):
     episodes = np.arange(len(rewards))
     mean = lfilter(b, a, rewards)
 
-    plt.plot(episodes, mean, color=color[c], linestyle='-', linewidth=2, label=directory)
-    plt.plot(episodes, rewards, color=color[c], linestyle='-', linewidth=1, alpha=0.25)  # label='Real Rewards'
+    plt.plot(episodes, mean, color=color[c], linestyle='-', linewidth=3, label=directory)
+    plt.plot(episodes, rewards, color=color[c], linestyle='-', linewidth=1, alpha=0.15)  # label='Real Rewards'
 
-plt.title('Reward per Episode', size=20)
-plt.legend(loc=4, prop={'size': 12})
-plt.xlabel('Episode')
-plt.ylabel('Reward')
-plt.xlim([0, 1000])
-plt.ylim([-21, 201])
-plt.show()
+    if (c+1) % 4 == 0:
+        plt.title('Reward per Episode', size=20)
+        plt.legend(loc=4, prop={'size': 12})
+        plt.xlabel('Episode')
+        plt.ylabel('Reward')
+        plt.xlim([0, 400])
+        plt.ylim([-21, 201])
+        plt.show()
--- view_results.py
+++ view_results.py
@@ -11,7 +11,7 @@ list_dir = os.listdir(path + '/results/')
 stage1 = mpimg.imread(path+'/media/stage_1.png')
 stage2 = mpimg.imread(path+'/media/stage_2.png')
 
-order = [True, False, True, True, False, False, True, False]
+order = [True, False, True, True, False, True, False, False]
 color = {0: 'firebrick', 1: 'tomato', 2: 'peru', 3: 'gold', 4: 'dodgerblue', 5: 'springgreen', 6: 'indigo', 7: 'deeppink'}
 for c, directory in tqdm(enumerate(list_dir), total=len(list_dir)):
     with open(path+'/results/'+directory+'/writer_data.json') as f:
@@ -19,32 +19,32 @@ for c, directory in tqdm(enumerate(list_dir), total=len(list_dir)):
 
     key_list = list(data.keys())
     new_key_list = ["/".join(key.split('/')[-2:]) for key in key_list]
-    print(new_key_list)
+
     for i, key in enumerate(key_list):
         data[new_key_list[i]] = data.pop(key)
 
     x = pd.DataFrame(data['agent_0/x']).iloc[:, 2].to_numpy().tolist()
     y = pd.DataFrame(data['agent_0/y']).iloc[:, 2].to_numpy().tolist()
 
-    plt.imshow(stage2 if order[c] else stage1, extent=[min(x) - 0.5, max(x) + 0.5, min(y) - 0.5, max(y) + 0.5])
+    plt.imshow(stage2 if order[c] else stage1, extent=[min(x) - 0.7, max(x) + 0.7, min(y) - 0.7, max(y) + 0.7] if
+               order[c] else [min(x) - 0.95, max(x) + 0.95, min(y) - 0.95, max(y) + 0.95])
 
-    new_x = []
-    new_y = []
+    new_x = list()
+    new_y = list()
     last = 0
     for i in range(len(x)-1):
-        if abs(x[i]) > 1 and abs(x[i+1]) <= 0.5:
-            new_x.append(x[last:i+1])
-            new_y.append(y[last:i+1])
+        if abs(x[i]) > 0.5 and abs(x[i+1]) <= 0.5:
+            new_x.append(x[last+1:i-1])
+            new_y.append(y[last+1:i-1])
             last = i+1
 
     for x, y in zip(new_x, new_y):
-        plt.plot(y, x, color=color[c], linestyle='-', linewidth=1)  # label=list_dir[0]
-        # plt.plot(episodes, rewards, color=color[0], linestyle='-', linewidth=1, alpha=0.25)
+        plt.plot(y, x, color=color[c], linestyle='-', linewidth=1)
 
     plt.title('Path '+directory, size=20)
-    plt.legend(loc=4, prop={'size': 12})
+    # plt.legend(loc=4, prop={'size': 12})
     plt.xlabel('Meters')
     plt.ylabel('Meters')
-    plt.xlim([-2, 2])
-    plt.ylim([-2, 2])
+    plt.xlim([-2.1, 2.1])
+    plt.ylim([-2.1, 2.1])
     plt.show()
