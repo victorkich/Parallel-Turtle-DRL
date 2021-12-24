e6d9db84ddc09d68aa0e5bbc060234403350496b
victorkich <victorkich@yahoo.com.br>
2021-12-24 11:20:31 -0300

    Update

--- algorithms/d4pg.py
+++ algorithms/d4pg.py
@@ -103,6 +103,7 @@ class LearnerD4PG(object):
         if self.prioritized_replay:
             weights_update = np.abs(td_error) + self.config['priority_epsilon']
             replay_priority_queue.put_nowait((inds, weights_update))
+            print('D4PG Prioritized')
             value_loss = value_loss * torch.tensor(weights).float().to(self.device)
 
         # Update step
