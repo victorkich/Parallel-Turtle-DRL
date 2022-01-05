135e468db1ffe55522734c6cfb71a3a43007e10a
victorkich <victorkich@yahoo.com.br>
2022-01-05 14:57:21 -0300

    Update

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ import cv2
 from sensor_msgs.msg import Image
 from utils.defisheye import Defisheye
 from cv_bridge import CvBridge
-import numpy as np
 import time
 
 img = None
@@ -22,12 +21,12 @@ defisheye = Defisheye(dtype='linear', format='fullframe', fov=160, pfov=130)
 bridge = CvBridge()
 
 while True:
-    print(img)
     if img is not None:
         frame = bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
+        frame = frame[:, 0:round(frame.shape[0]*0.8)]
         frame = defisheye.convert(frame)
         # Display the resulting frame
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    time.sleep(0.1)
+    time.sleep(0.05)
