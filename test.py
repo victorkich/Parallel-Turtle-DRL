317438198c8b8569a79623d274710a144711daba
victorkich <victorkich@yahoo.com.br>
2022-01-05 14:54:20 -0300

    Update

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import rospy
 import cv2
 from sensor_msgs.msg import Image
 from utils.defisheye import Defisheye
+from cv_bridge import CvBridge
 import numpy as np
 import time
 
@@ -12,18 +13,18 @@ img = None
 
 def getImage(im):
     global img
-    img = im.data
+    img = im
 
 
 rospy.init_node('test')
 sub_image = rospy.Subscriber('/usb_cam/image_raw', Image, getImage, queue_size=1)
 defisheye = Defisheye(dtype='linear', format='fullframe', fov=160, pfov=130)
-
+bridge = CvBridge()
 
 while True:
     print(img)
     if img is not None:
-        frame = np.frombuffer(img, np.uint8)
+        frame = bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
         frame = defisheye.convert(frame)
         # Display the resulting frame
         cv2.imshow('frame', frame)
