3724dedfd7eb911d4b3be20f2f9c5f61ebf6a05c
victorkich <victorkich@yahoo.com.br>
2022-01-10 13:57:16 -0300

    Update

--- utils/range_finder.py
+++ utils/range_finder.py
@@ -82,8 +82,8 @@ class RealTtb:
         self.greenUpper = (130, 255, 255)  # (128, 255, 203)
         self.redLower = (141, 50, 90)  # (141, 50, 90)
         self.redUpper = (220, 255, 255)  # (220, 255, 255)
-        self.yellowLower = (58, 50, 100)
-        self.yellowUpper = (100, 120, 255)
+        self.yellowLower = (58, 50, 0)
+        self.yellowUpper = (100, 120, 150)
 
         fourcc = cv2.VideoWriter_fourcc(*'MP4V')
         self.out = cv2.VideoWriter(data_dir+archive+'.mp4', fourcc, 24.0, output, True)
