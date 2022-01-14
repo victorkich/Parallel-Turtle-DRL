6e10770443fbf1260ab5002df537b491570bf9aa
victorkich <victorkich@yahoo.com.br>
2022-01-14 16:31:19 -0300

    Update

--- utils/range_finder.py
+++ utils/range_finder.py
@@ -79,7 +79,7 @@ class RealTtb:
         self.blueLower = (58, 50, 100)  # (26, 200, 100)  # (58, 108, 199)
         self.blueUpper = (100, 120, 255)  # (128, 255, 255)  # (136, 255, 255)
         self.greenLower = (100, 100, 50)  # (26, 200, 40)
-        self.greenUpper = (200, 225, 255)  # (128, 255, 203)
+        self.greenUpper = (200, 255, 255)  # (128, 255, 203)
         self.redLower = (141, 50, 90)  # (141, 50, 90)
         self.redUpper = (220, 255, 255)  # (220, 255, 255)
         self.yellowLower = (100, 100, 50)
