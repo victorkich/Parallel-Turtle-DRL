6afa062dab766eddfb1bd4124ed9add577541701
victorkich <victorkich@yahoo.com.br>
2022-02-03 14:28:16 -0300

    Update

--- test_real.py
+++ test_real.py
@@ -99,7 +99,7 @@ while True:
         process_dir = f"{path}/saved_models/{translator[int(algorithm)][0]}_{config['dense_size']}_A{config['num_agents']}_S{env}_{'P' if config['replay_memory_prioritized'] else 'N'}"
         list_dir = sorted(os.listdir(process_dir))
         model_fn = f"{process_dir}/{list_dir[-2]}"
-        print('Loaded:', model_fn)
+        print('Loaded:', list_dir[-2])
 
         # Loading neural network model
         if any(algorithm == algorithms_sel[[0, 2]]):
