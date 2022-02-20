44ac15a7537f9a201c9fc5c5ee4bc26565f9db3f
victorkich <victorkich@yahoo.com.br>
2022-02-20 16:48:23 -0300

    Update

--- utils/utils.py
+++ utils/utils.py
@@ -509,7 +509,7 @@ def test_goals(t):
         return [0.5, -3.5]
     elif t >= 75:
         return [3.5, -3.5]
-"""
+
 def test_goals(t):
     if t < 25:
         return [-1.5, -1.5]
@@ -529,7 +529,7 @@ def test_goals(t):
         return [-1.5, 1.5]
     elif t >= 4:
         return [1.5, 1.5]
-"""
+
 """
 def test_goals(t):
     if t < 2:
