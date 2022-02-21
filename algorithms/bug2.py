c4f0e9245e7d516fc4ecf3835c3a852b7e008598
victorkich <victorkich@yahoo.com.br>
2022-02-21 17:01:08 -0300

    Update

--- algorithms/bug2.py
+++ algorithms/bug2.py
@@ -11,7 +11,7 @@ class BUG2:
         self.flag_1 = 0
         self.dist = 0.0
         self.first = True
-        self.colission_distance = 0.18 * 2
+        self.colission_distance = 0.22 * 2
 
     def angle_towards_goal(self, angle):
         difference_angle = angle
