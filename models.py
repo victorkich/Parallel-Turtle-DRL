675f895742005b70273cc47ce8860574b0c8ea99
victorkich <victorkich@yahoo.com.br>
2022-03-31 15:48:50 -0300

    Update

--- models.py
+++ models.py
@@ -79,6 +79,8 @@ class PolicyNetwork(nn.Module):
             state = pad_sequence(state, batch_first=True)
             state = state.view(batch_size, seq_size, obs_size)
             hxs = hxs.view(batch_size, seq_size, -1)
+            print('State:', state)
+            print('Hxs:', hxs)
             x, hx = self.lstm(state, hxs)
             x = torch.relu(x)
             x = torch.tanh(self.linear1(x))
