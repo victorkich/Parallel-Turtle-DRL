d006acd57d0189125fff31f0ca489cced18ba83b
victorkich <victorkich@yahoo.com.br>
2022-04-25 12:43:33 -0300

    Update

--- algorithms/d4pg.py
+++ algorithms/d4pg.py
@@ -157,7 +157,7 @@ class LearnerD4PG(object):
     def run(self, training_on, batch_queue, replay_priority_queue, update_step, global_episode, logs):
         torch.set_num_threads(4)
         pbar = tqdm(total=self.config['num_steps_train'])
-        while update_step.value <= self.config['num_steps_train']:
+        while update_step.value < self.config['num_steps_train']:
             try:
                 batch = batch_queue.get_nowait()
             except queue.Empty:
