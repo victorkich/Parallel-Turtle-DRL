8a0378676d1721963feb4e8285888abf0367c13a
victorkich <victorkich@yahoo.com.br>
2022-04-29 16:37:39 -0300

    Update

--- models.py
+++ models.py
@@ -412,7 +412,6 @@ class TanhGaussianPolicy(Mlp, metaclass=abc.ABCMeta):
                        c_0.clone().detach().to(self.device).view(batch_size, seq_size, -1)[:, 0, :].view(1, batch_size, self.hidden_size).contiguous())
             else:
                 seq_size = 1
-                batch_size = 1
                 if h_0 is None and c_0 is None:
                     h_0 = torch.zeros((1, self.hidden_sizes[0]))
                     c_0 = torch.zeros((1, self.hidden_sizes[0]))
@@ -420,8 +419,10 @@ class TanhGaussianPolicy(Mlp, metaclass=abc.ABCMeta):
                     h_0 = torch.Tensor(h_0)
                     c_0 = torch.Tensor(c_0)
 
-                hxs = (h_0.clone().detach().to(self.device).view(seq_size, -1)[:, 0, :].view(1, self.hidden_size).contiguous(),
-                       c_0.clone().detach().to(self.device).view(seq_size, -1)[:, 0, :].view(1, self.hidden_size).contiguous())
+                print(h_0.shape)
+                print(c_0.shape)
+                hxs = (h_0.clone().detach().to(self.device).view(seq_size, -1)[0, :].view(1, self.hidden_size).contiguous(),
+                       c_0.clone().detach().to(self.device).view(seq_size, -1)[0, :].view(1, self.hidden_size).contiguous())
 
         for i, fc in enumerate(self.fcs):
             if self.recurrent and not i:
