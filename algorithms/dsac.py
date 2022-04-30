23e24bcd5d21e683ae5701951aa84814bea70e21
victorkich <victorkich@yahoo.com.br>
2022-04-30 14:04:37 -0300

    Update

--- algorithms/dsac.py
+++ algorithms/dsac.py
@@ -160,7 +160,7 @@ class LearnerDSAC(object):
             replay_priority_queue.put((inds, weights_update))
             if self.config['recurrent_policy']:
                 w_shape = weights.shape[0]
-                weights = weights.view(1, 1, w_shape)
+                weights = weights.reshape((1, 1, w_shape))
             print(weights.shape)
             value_loss_1 = zf1_loss * torch.tensor(weights).float().to(self.device)
             value_loss_2 = zf2_loss * torch.tensor(weights).float().to(self.device)
