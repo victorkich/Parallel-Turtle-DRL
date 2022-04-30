ee3fba516ba9ed173d4bbbb630ae1f86254effb9
victorkich <victorkich@yahoo.com.br>
2022-04-30 15:24:31 -0300

    Update

--- algorithms/d4pg.py
+++ algorithms/d4pg.py
@@ -102,14 +102,16 @@ class LearnerD4PG(object):
         critic_value = critic_value.to(self.device)
 
         value_loss = self.value_criterion(critic_value, target_z_projected)
-        value_loss = value_loss.mean(axis=2 if self.config['recurrent_policy'] else 1)
+        value_loss = value_loss.mean(axis=1)
 
         # Update priorities in buffer
-        td_error = value_loss.cpu().detach().numpy().flatten()
-
         if self.prioritized_replay:
+            td_error = value_loss.cpu().detach().numpy().flatten()
             weights_update = np.abs(td_error) + self.config['priority_epsilon']
             replay_priority_queue.put((inds, weights_update))
+            if self.config['recurrent_policy']:
+                w_shape = weights.shape[0]
+                weights = weights.reshape((w_shape, 1))
             value_loss = value_loss * torch.tensor(weights).float().to(self.device)
 
         # Update step
--- algorithms/dsac.py
+++ algorithms/dsac.py
@@ -149,16 +149,12 @@ class LearnerDSAC(object):
         z2_pred = self.zf2(obs, actions, tau_hat)
         zf1_loss = self.zf_criterion(z1_pred, z_target, tau_hat, next_presum_tau)
         zf2_loss = self.zf_criterion(z2_pred, z_target, tau_hat, next_presum_tau)
-        zf1_loss = zf1_loss.mean(axis=2 if self.config['recurrent_policy'] else 1)
-        zf2_loss = zf2_loss.mean(axis=2 if self.config['recurrent_policy'] else 1)
+        zf1_loss = zf1_loss.mean(axis=1)
+        zf2_loss = zf2_loss.mean(axis=1)
 
         # Update priorities in buffer 1
-        value_loss = torch.min(zf1_loss, zf2_loss)
         if self.prioritized_replay:
-            if self.config['recurrent_policy']:
-                zf1_loss = zf1_loss.mean(axis=1)
-                zf2_loss = zf2_loss.mean(axis=1)
-                value_loss = torch.min(zf1_loss, zf2_loss)
+            value_loss = torch.min(zf1_loss, zf2_loss)
             td_error = value_loss.cpu().detach().numpy().flatten()
             weights_update = np.abs(td_error) + self.config['priority_epsilon']
             replay_priority_queue.put((inds, weights_update))
