9f22b9117f78d0ab3ee6c7ae079ba1e8a467f25d
victorkich <victorkich@yahoo.com.br>
2022-05-03 11:54:35 -0300

    Update

--- algorithms/dsac.py
+++ algorithms/dsac.py
@@ -150,9 +150,12 @@ class LearnerDSAC(object):
         zf2_loss = zf2_loss.mean(axis=1)
         value_loss = torch.min(zf1_loss, zf2_loss)
 
+        print(value_loss.shape)
+        print(inds.shape)
         # Update priorities in buffer 1
         if self.prioritized_replay:
-            value_loss = torch.min(zf1_loss, zf2_loss)
+            if self.config['recurrent_policy']:
+                value_loss = value_loss.mean(axis=1)
             td_error = value_loss.cpu().detach().numpy().flatten()
             weights_update = np.abs(td_error) + self.config['priority_epsilon']
             replay_priority_queue.put((inds, weights_update))
