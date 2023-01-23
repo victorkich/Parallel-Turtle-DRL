8fd0e8a9cf11df751a2b1af0611ffad67446b9f5
victorkich <victorkich@yahoo.com.br>
2023-01-23 11:02:09 -0300

    Debugging DDPG

--- agent.py
+++ agent.py
@@ -97,11 +97,11 @@ class Agent(object):
             while not done and training_on.value:
                 if self.config['obs_noise']:
                     state += np.random.normal(loc=0.0, scale=0.05, size=len(state))
-                for s in range(len(state)):
-                    if state[s] > 3.5:
-                        state[s] = 3.5
-                    if state[s] < 0.0:
-                        state[s] = 0.0
+                # for s in range(len(state)):
+                #    if state[s] > 3.5:
+                #        state[s] = 3.5
+                #    if state[s] < 0.0:
+                #        state[s] = 0.0
 
                 if self.config['model'] == 'PDSRL' or self.config['model'] == 'SAC':
                     action, hx = self.actor.get_action(torch.Tensor(state).to(self.config['device']), h_0=h_0, c_0=c_0,
