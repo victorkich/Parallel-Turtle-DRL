938aea700b7dfb61d26cc3e0cad9b93814dc0c06
victorkich <victorkich@yahoo.com.br>
2023-02-06 14:17:48 -0300

    Preparing real environment for the models evaluation

--- test_real.py
+++ test_real.py
@@ -3,7 +3,7 @@
 import rospy
 from utils import range_finder as rf
 import gym_turtlebot3
-from models import PolicyNetwork, TanhGaussianPolicy
+from models import PolicyNetwork, TanhGaussianPolicy, DiagGaussianActor
 from utils.defisheye import Defisheye
 from algorithms.bug2 import BUG2
 from sensor_msgs.msg import Image
@@ -23,11 +23,15 @@ import os
 TURTLE = '003'
 bridge = CvBridge()
 state = None
+frame = None
 font = cv2.FONT_HERSHEY_SIMPLEX
 outfile = TemporaryFile()
+RECORD = False
+if RECORD:
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
 # Hyper parameters
-episodes = 50
+episodes = 12
 max_steps = 500
 action_low = [-1.5, -0.1]
 action_high = [1.5, 0.12]
@@ -41,12 +45,13 @@ env = input('Which environment are you running? [1 | 2 | l | u]:\n')
 rospy.init_node(config['env_name'].replace('-', '_') + "_test_real")
 env_real = gym.make(config['env_name'], env_stage=env.lower(), observation_mode=0, continuous=True, test_real=True)
 _ = env_real.reset()
-real_ttb = rf.RealTtb(config, path, output=(640, 640))
+real_ttb = rf.RealTtb(config, path, output=(720, 720))
 defisheye = Defisheye(dtype='linear', format='fullframe', fov=100, pfov=90)
 
 
 def getImage(image):
     global state
+    global frame
     try:
         lidar = rospy.wait_for_message('scan_' + TURTLE, LaserScan, timeout=1)
     except:
@@ -87,7 +92,7 @@ while True:
     algorithm = ""
     while not any(algorithm.lower() == algorithms_sel):
         print('Choose the algorithm or exit the test:')
-        algorithm = input('1->PDDRL | 2->PDSRL | 3->PDDRL-P | 4->PDSRL-P | 5->DDPG | 6->SAC | 7->BUG2 | '
+        algorithm = input('1->PDDRL | 2->PDSRL | 3->PDDRL-P | 4->PDSRL-P | 5->DDPG | 6->SAC | 7->DDPG-P | 8->SAC-P | 9->BUG2 | '
                           'e->exit | r->reset\n')
     if algorithm.lower() == 'e':
         break
@@ -95,22 +100,24 @@ while True:
         real_ttb.cleanPath()
         continue
 
-    if algorithm != '7':
-        process_dir = f"{path}/saved_models/{translator[int(algorithm)][0]}_{config['dense_size']}_A{config['num_agents']}_S{env}_{'P' if (algorithm == '3' or algorithm == '4') else 'N'}"
-        # list_dir = sorted(os.listdir(process_dir))
-        list_dir = "local_episode_1000_reward_200.000000.pt"
+    if algorithm != '9':
+        process_dir = f"{path}/saved_models/{translator[int(algorithm)][0]}_{config['dense_size']}_A{config['num_agents']}_S{env}_{'P' if any(algorithm == algorithms_sel[[2, 3, 6, 7]]) else 'N'}"
+        list_dir = sorted(os.listdir(process_dir))[-2]
         model_fn = f"{process_dir}/{list_dir}"
-        #for i, l in enumerate(list_dir):
+        # for i, l in enumerate(list_dir):
         #    print(i, l)
 
-        #print('Loaded:', list_dir[0])
+        print('Loaded:', list_dir[-2])
 
         # Loading neural network model
-        if any(algorithm == algorithms_sel[[0, 2]]):
+        if any(algorithm == algorithms_sel[[0, 2, 4, 6]]):
             actor = PolicyNetwork(config['state_dim'], config['action_dim'], config['dense_size'], device=config['device'])
         elif any(algorithm == algorithms_sel[[1, 3]]):
             actor = TanhGaussianPolicy(config=config, obs_dim=config['state_dim'], action_dim=config['action_dim'],
                                        hidden_sizes=[config['dense_size'], config['dense_size']])
+        elif algorithm == algorithms_sel[5, 7]:
+            actor = DiagGaussianActor(config['state_dim'], config['action_dim'], config['dense_size'], 2,
+                                                  [-config['max_action'], config['max_action']])
         try:
             actor.load_state_dict(torch.load(model_fn, map_location=config['device']))
         except:
@@ -120,35 +127,39 @@ while True:
     else:
         b2 = BUG2()
 
-    local_episode = 0
-    while local_episode < episodes:
-        if local_episode == 0:
-            quit = input("Press [Enter] to start the test or press [q] to quit...")
-        else:
-            quit = input("Press [Enter] to continue to the next episode or press [q] to quit...")
-        if quit.lower() == 'q':
+    slots = [None] * episodes
+    while True:
+        print('Current Slots:')
+        for i, slot in enumerate(slots):
+            print(i, '- slot:', slot)
+        value = input("Press [slot number] to start the test or press [q] to quit...")
+        if value.lower() == 'q':
             break
 
         real_ttb.cleanPath()
         episode_reward = 0
         lidar_list = list()
         num_steps = 0
-        local_episode += 1
         ep_start_time = time.time()
         done = False
+        if RECORD:
+            out = cv2.VideoWriter(path_results + '/{}_{}_S{}_episode{}.mp4'.format(translator[int(algorithm)][0],
+                                                                                   translator[int(algorithm)][1],
+                                                                                   env, value), fourcc, 20.0, (720, 720))
+            out.write(frame)
         while True:
             start = time.time()
             print('Num steps:', num_steps)
-            if state is not None:
-                for s in range(len(state)):
-                    if state[s] > 2.5:
-                        state[s] = 2.5
+            # if state is not None:
+            #    for s in range(len(state)):
+            #        if state[s] > 2.5:
+            #            state[s] = 2.5
 
             print('State:', state)
             # state[:24] = list(reversed(state[:24]))
             # state[-2] = -state[-2]
 
-            if algorithm != '7':
+            if algorithm != '9':
                 if algorithm == '2' or algorithm == '4':
                     action, _, _, _, _, _, _, _ = actor.forward(torch.Tensor(state).to(config['device']), deterministic=True)
                 else:
@@ -163,17 +174,19 @@ while True:
             # action[0] /= 2
             # action[1] /= 1.2
             _, _, _, _ = env_real.step(action=action)
+            if RECORD:
+                out.write(frame)
             done = False
             reward = 0
-            for i in range(len(state[:24])):
-                if state[i] == 0.0:
-                    state[i] = 1.0
+            # for i in range(len(state[:24])):
+            #    if state[i] == 0.0:
+            #        state[i] = 1.0
             if state[-1] < 0.3:
                 done = True
-                reward = 20
+                reward = 200
             if 0.1 < min(state[0:24]) < 0.14:
                 done = True
-                reward = -200
+                reward = -20
             episode_reward += reward
 
             scan = state[0:24]
@@ -191,11 +204,14 @@ while True:
 
         # Log metrics
         episode_timing = time.time() - ep_start_time
-        print(f"Agent: [Test] Episode: [{local_episode}/{episodes}] Reward: [{episode_reward}/20] "
+        print(f"Agent: [Test] Episode: [{value}/{episodes}] Reward: [{episode_reward}/20] "
               f"Steps: [{num_steps}/{max_steps}] Episode Timing: {round(episode_timing, 2)}s")
 
         # Save log file
-        values = [episode_reward, episode_timing, local_episode, num_steps, real_ttb.pts, lidar_list]
-        with open(path_results + '/{}_{}_S{}_episode{}'.format(translator[int(algorithm)][0], translator[int(algorithm)][1], env, local_episode), "wb") as fp:
+        values = [episode_reward, episode_timing, value, num_steps, real_ttb.pts, lidar_list]
+        with open(path_results + '/{}_{}_S{}_episode{}.pkl'.format(translator[int(algorithm)][0],
+                                                                   translator[int(algorithm)][1], env, value), "wb") as fp:
             pickle.dump(values, fp)
+        if RECORD:
+            out.release()
     print('Episode done!')
