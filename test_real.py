de99a0389d3504b50a8eb7e477454bca47dd79ad
victorkich <victorkich@yahoo.com.br>
2023-02-08 14:47:05 -0300

    Preparing real environment for the models evaluation

--- test_real.py
+++ test_real.py
@@ -74,6 +74,7 @@ def get_state(return_old=False):
 
         if not angle is None and not distance is None:
             state = np.hstack([lidar, angle, distance])
+            old_state = state
     return state
 
 def getImage(img):
