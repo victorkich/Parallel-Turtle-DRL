31ae422f8fb7116ab7440d769eb0f4cf5fb73119
victorkich <victorkich@yahoo.com.br>
2023-02-08 14:02:41 -0300

    Preparing real environment for the models evaluation

--- utils/range_finder.py
+++ utils/range_finder.py
@@ -82,8 +82,8 @@ class RealTtb:
         self.greenUpper = (48, 143, 236)
         self.redLower = (147, 0, 189)  # left
         self.redUpper = (179, 127, 255)
-        self.yellowLower = (170, 76, 159) # target
-        self.yellowUpper = (179, 132, 209)
+        self.yellowLower = (142, 99, 0) #(170, 76, 159) # target
+        self.yellowUpper = (179, 255, 255) # (179, 132, 209)
 
         fourcc = cv2.VideoWriter_fourcc(*'MP4V')
         # self.out = cv2.VideoWriter(data_dir+archive+'.mp4', fourcc, 24.0, output, True)
