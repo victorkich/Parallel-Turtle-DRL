630f4684ca53bc9a650c65a161b4be00d5c21454
victorkich <victorkich@yahoo.com.br>
2023-02-10 17:56:14 -0300

    Preparing real environment for the models evaluation

--- test_real.py
+++ test_real.py
@@ -211,8 +211,8 @@ while True:
             action[1] = np.clip(action[1], action_low[1], action_high[1])
 
             print('Action:', action)
-            action[0] *= 0.75
-            action[1] *= 0.75
+            action[0] *= 0.8
+            action[1] *= 0.8
             _, _, _, _ = env_real.step(action=action)
             if RECORD:
                 out.write(frame)
