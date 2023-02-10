ece9c236fff05829240fafefd2384a11c2502ccf
victorkich <victorkich@yahoo.com.br>
2023-02-10 18:26:47 -0300

    Preparing real environment for the models evaluation

--- test_real.py
+++ test_real.py
@@ -226,7 +226,7 @@ while True:
                 done = True
                 reward = 200
 
-            state_idx = (state > 0.1) * (state < 0.15)
+            state_idx = (state > 0.13) * (state < 0.18)
             if len(state[state_idx]):
                 done = True
                 reward = -20
