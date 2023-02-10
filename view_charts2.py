abf76fa1e2a1f3342fdd7856d9eac923bbb669e1
victorkich <victorkich@yahoo.com.br>
2023-02-09 23:27:44 -0300

    Preparing real environment for the models evaluation

--- view_charts2.py
+++ view_charts2.py
@@ -7,6 +7,21 @@ import json
 import os
 
 
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32,
+                              np.float64)):
+            return float(obj)
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+
 def mfilter(array, size):
     numbers = array
     window_size = size
@@ -35,7 +50,8 @@ n = 100  # the larger n is, the smoother curve will be
 b = [1.0 / n] * n
 a = 1
 
-color = {'DDPG-N': 'dodgerblue', 'DDPG-P': 'springgreen', 'SAC-N': 'indigo', 'SAC-P': 'deeppink'}
+color = {'PDDRL-N': 'dodgerblue', 'PDSRL-N': 'springgreen', 'PDDRL-P': 'indigo', 'PDSRL-P': 'deeppink',
+         'DDPG-N': 'orange', 'DDPG-P': 'black', 'SAC-N': 'azure', 'SAC-P': 'brown'}
 x_lim = {'S1': 100000, 'S2': 200000, 'Sl': 200000, 'Su': 200000, 'SL': 200000}
 fig, ax = plt.subplots()
 
@@ -44,7 +60,10 @@ for c, directory in tqdm(enumerate(sorted_dir), total=len(sorted_dir)):
     directory = '_'.join(directory)+'.json'
     print(path+directory)
     with open(path+directory) as f:
-        data = json.load(f)[0]
+        if any(directory == np.array(['DDPG-N', 'DDPG-P', 'SAC-N', 'SAC-P'])):
+            data = json.load(f)
+        else:
+            data = json.load(f)
 
     rewards = data['y']
     rewards = np.array([200 if reward >= 200 else reward for reward in rewards])
@@ -55,6 +74,7 @@ for c, directory in tqdm(enumerate(sorted_dir), total=len(sorted_dir)):
 
     # sel = '-'.join([directory[0], directory[4]])
     sel = sorted_dir[c]
+    print(sel)
     ax.plot(steps, means, linestyle='-', linewidth=2, label='-'.join(sel[:-1]), c=color['-'.join(sel[:-1])])
     ax.fill_between(steps, means - stds, means + stds, alpha=0.15, facecolor=color['-'.join(sel[:-1])])
 
@@ -71,4 +91,73 @@ for c, directory in tqdm(enumerate(sorted_dir), total=len(sorted_dir)):
         fig, ax = plt.subplots()
 
     del data
+
+########################################################################################################################
+"""
+path = os.path.dirname(os.path.abspath(__file__)) + '/saved_models/'
+list_dir = os.listdir(path)
+splitted_dir = list()
+for dir in list_dir:
+    splitted_dir.append(dir.split('_'))
+sorted_dir = sorted(splitted_dir, key=lambda row: row[3])
+
+n = 100  # the larger n is, the smoother curve will be
+b = [1.0 / n] * n
+a = 1
+
+color = {'PDDRL-N': 'dodgerblue', 'PDSRL-N': 'springgreen', 'PDDRL-P': 'indigo', 'PDSRL-P': 'deeppink'}
+x_lim = {'S1': 150, 'S2': 500, 'Sl': 2000, 'Su': 2000}
+fig, ax = plt.subplots()
+
+# sorted_dir = sorted_dir[-8:-4]
+print('Dir:', sorted_dir)
+
+print('Generating charts...')
+for c, directory in tqdm(enumerate(sorted_dir), total=len(sorted_dir)):
+    print(path + '_'.join(directory) + '/writer_data.json')
+    with open(path + '_'.join(directory) + '/writer_data.json') as f:
+        data = json.load(f)
+
+    print('Directory:', directory)
+    key_list = list(data.keys())
+    new_key_list = ["/".join(key.split('/')[-2:]) for key in key_list]
+
+    for i, key in enumerate(key_list):
+        data[new_key_list[i]] = data.pop(key)
+
+    # print(data)
+    rewards = pd.DataFrame(data['agent_0/reward']).iloc[:, 2].to_numpy()
+    rewards = np.array([200 if reward >= 200 else reward for reward in rewards])
+    # steps = pd.DataFrame(data['data_struct/global_step']).iloc[:, 2].to_numpy()
+    episodes = np.arange(len(rewards))
+
+    directory_name = np.array(directory)
+    dumped = json.dumps({'x': episodes, 'y': rewards}, cls=NumpyEncoder)
+    with open('_'.join(directory_name[[0, -2, -1]])+'.json', 'a') as f:
+        f.write(dumped + '\n')
+
+    #with open('_'.join(directory[[0, -2, -1]])+'.json', 'w') as outfile:
+    #    json.dump({'x': episodes, 'y': rewards}, outfile)
+
+    means = lfilter(b, a, rewards)
+    _, stds = mfilter(rewards, n)
+
+    sel = '-'.join([directory[0], directory[4]])
+    ax.plot(episodes, means, linestyle='-', linewidth=2, label=sel if sel.split('-')[1] == 'P' else sel.split('-')[0],
+            c=color[sel])
+    ax.fill_between(episodes, means - stds, means + stds, alpha=0.15, facecolor=color[sel])
+
+    if (c + 1) % 4 == 0:
+        ax.legend(loc=4, prop={'size': 14})
+        ax.set_xlabel('Episode', fontsize=12)
+        ax.set_ylabel('Reward', fontsize=12)
+        ax.set_xlim([0, x_lim[directory[3]]])
+        ax.set_ylim([-21, 201])
+        ax.grid()
+        # plt.savefig("{}.pdf".format(sel), format="pdf", bbox_inches="tight", backend='pgf')
+        plt.show()
+        fig, ax = plt.subplots()
+
+    del data
 print('Done!')
+"""
