6486021a35eeda3a728521723951428efb19980b
victorkich <victorkich@yahoo.com.br>
2023-02-13 15:26:17 -0300

    Preparing real environment for the models evaluation

--- utils/utils.py
+++ utils/utils.py
@@ -508,7 +508,7 @@ def eval_np(module, *args, **kwargs):
     else:
         return np_ify(outputs)
 
-
+"""
 def test_goals(t):
     if t < 25:
         return [2.5, -2.0]
@@ -518,8 +518,8 @@ def test_goals(t):
         return [0.5, -3.5]
     elif t >= 75:
         return [3.5, -3.5]
-
 """
+
 def test_goals(t):
     if t < 25:
         return [-1.5, -1.5]
@@ -530,7 +530,7 @@ def test_goals(t):
     elif t >= 75:
         return [1.5, 1.5]
 
-
+"""
 def test_goals(t):
     if t < 2:
         return [-1.5, -1.5]
