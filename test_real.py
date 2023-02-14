55019b270ba56ce905810a611de29c459fe73c3b
victorkich <victorkich@yahoo.com.br>
2023-02-14 17:39:50 -0300

    Preparing real environment for the models evaluation

--- test_real.py
+++ test_real.py
@@ -182,12 +182,12 @@ while True:
             print('Num steps:', num_steps)
             if state is not None:
                 if not any(algorithm == algorithms_sel[[4, 5, 6, 7]]):
-                    #if state[-1] > 2.5:
-                    #    state[-1] = 2.5
+                    if state[-1] > 2.5:
+                        state[-1] = 2.5
                     pass
-                    #for s in range(0, len(state)-2):
-                    #    if state[s] > 2.5:
-                    #        state[s] = 2.5
+                    for s in range(0, len(state)-2):
+                        if state[s] > 2.5:
+                            state[s] = 2.5
                 else:
                     for s in range(0, len(state)-2):
                         if state[s] > 3.5:
